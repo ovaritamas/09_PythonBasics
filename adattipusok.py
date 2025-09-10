1474f69008d485400171bd5b9f3b3d89ec04ea9e
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-10 10:06:02 +0200

    3.ora_haladas

--- adattipusok.py
+++ adattipusok.py
@@ -1,6 +1,13 @@
 print("These are your basic information:")
 name = "Óvári Tamás"
-age = 17
-height = 1.80 #meters
+age = input("Enter your age: ") 
+height = input("Enter your height: ") #meters
 is_student = True
-print(f"My name is {name}, I am {age} years old, my height is {height} meters and it is {is_student} that I am a student.")
+print(f"My name is {name}, \nMy age: {age} years old, \nMy height is {height} meters and \nIt is {is_student} that I am a student.")
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
