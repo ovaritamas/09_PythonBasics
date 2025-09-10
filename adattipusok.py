af31d4687494fbcb0de4a5577af1416b5e50c09d
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-10 09:01:20 +0200

    basic_informations

---
+++ adattipusok.py
@@ -0,0 +1,6 @@
+print("These are your basic information:")
+name = "Óvári Tamás"
+age = 17
+height = 1.80 #meters
+is_student = True
+print(f"My name is {name}, I am {age} years old, my height is {height} meters and it is {is_student} that I am a student.")
