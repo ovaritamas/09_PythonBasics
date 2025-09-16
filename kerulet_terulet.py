b4a95ed366a1de309cdb4bc36c9c1d1f050a61d8
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-16 17:50:55 +0200

    szilo_gyak

---
+++ kerulet_terulet.py
@@ -0,0 +1,12 @@
+# Triangle area and circumfence calculation
+
+a = float(input("Give the a side of the triangle: "))
+b = float(input("Give the b side of the triangle: "))
+c = float(input("Give the c side of the triangle: "))
+ma = float(input("Give the ma of the triangle: "))
+
+area = a + b + c
+circumfence = a * ma / 2 
+
+print(f"The area of the triangle: {area}")
+print(f"The circumfence of the triangle: {circumfence}")
---
+++ szilo_gyakorlas.py
@@ -0,0 +1,10 @@
+# this code will calculate the circle's area
+
+PI = 3.14592
+r = 5 #cm
+
+kerület = 2 * PI *r
+terület = PI * r**2 
+
+print(f"kör kerülete: {kerület}")
+print(f"kör területe: {terület}")
