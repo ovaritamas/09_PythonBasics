e15e5e79ca5543f5496eba5daa27c617584df08f
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-17 09:00:50 +0200

    Update 04_haromszam.py

--- feladatok/04_elagazasok/04_haromszam.py
+++ feladatok/04_elagazasok/04_haromszam.py
@@ -6,9 +6,9 @@ first_num = int(input("Add meg az első számot: "))
 second_num = int(input("Add meg az második számot: "))
 third_num = int(input("Add meg az harmadik számot: "))
 
-if first_num > second_num and third_num:
-    print("Az első szám a legnagyobb")
-elif second_num > first_num and third_num:
+if first_num > second_num and first_num > third_num:
+    print(f"Az első szám a legnagyobb")
+elif second_num > first_num and second_num > third_num:
     print("A második szám a legnagyobb")
-else:
+elif third_num > first_num and third_num > second_num:
     print("A harmadik szám a legnagyobb")
