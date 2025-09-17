ec20f9a079da13a9717367d9c0b1b57e1e06168b
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-17 07:28:07 +0200

    Update kerulet_terulet.py

--- kerulet_terulet.py
+++ kerulet_terulet.py
@@ -3,7 +3,7 @@
 a = float(input("Give the a side of the triangle: "))
 b = float(input("Give the b side of the triangle: "))
 c = float(input("Give the c side of the triangle: "))
-ma = float(input("Give the ma of the triangle: "))
+ma = float(input("Give the ma of the triangle: ")) #height from a side
 
 area = a + b + c
 circumfence = a * ma / 2 
