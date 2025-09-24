2835d173accd2f3a27471d5a2bd896e318d07b51
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-24 09:56:40 +0200

    3class_upd

---
+++ feladatok/05_ciklusok/01.py
@@ -0,0 +1,10 @@
+
+# while magyarul azt jelenti: amíg  
+szam = 1
+while szam <= 5:
+    print(szam * '* ')
+    szam = szam + 1   
+
+while szam >= 1:
+    print(szam * '* ')   
+    szam = szam - 1
