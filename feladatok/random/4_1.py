81bfe30f61cdf2182af630a97fe4e1717ad6752d
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-09-24 09:26:49 +0200

    4_1_random

---
+++ feladatok/random/4_1.py
@@ -0,0 +1,21 @@
+"""
+Készíts egy rövid programot, amely 1 és 3 között generál véletlenszámot, 
+majd összehasonlítja ezt a felhasználó által megadott, szintén ebbe a tartományba eső számmal!
+Az összehasonlítás eredményéről tájékoztassa a felhasználót! 
+"""
+
+import random
+
+veletlen_szam = random.randint(1, 3)
+
+megadott_szam = int(input("Gondolj egy számra (1-3): "))
+
+if veletlen_szam == megadott_szam:
+    print("Eltaláltad, gratulálok!")
+
+elif veletlen_szam > megadott_szam:
+    print("Az úgy karcsú lesz!")
+
+else:
+    print("Sok lesz, kicsit kevesebbet")
+
